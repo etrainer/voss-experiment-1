bb020bb81a46e872a993cf955387367a7498227a
Erik Trainer <etrainer@gmail.com>
2015-07-30 08:40:00 -0400

    Refactored submit.php

--- 3/submit.php
+++ 3/submit.php
@@ -1,23 +1,19 @@
 <?php
   
 
-    $fname = $_POST["filename"];
-    if (isset($_POST["filename"])) {
+    if (isset($_POST["filename"]) && isset($_POST["historyItem"])) {
+        $fname = $_POST["filename"];
         $myfile = fopen("$fname" . " edited.txt", "w") or die ("Unable to open file!");
-
         $history = $_POST["historyItem"]; 
-        if (isset($_POST["historyItem"])) {
-            foreach ($history as $item) {
-                fwrite($myfile, $item . "\n");
-            }
-            echo "<h1>Data submitted successfully!</h1>";
-        }   
-        else {
+
+        foreach ($history as $item) {
+            fwrite($myfile, $item . "\n");
         }
+        echo "<h1>Data submitted successfully!</h1>";
         fclose($myfile);
     }
     else
-        echo "The file name is not set!";
+        echo "There was an error!";
     
 ?>
 

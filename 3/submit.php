ff2be9fbaa3a8d85a816ffd54a9691f8259639c6
Erik Trainer <etrainer@gmail.com>
2015-07-30 09:59:57 -0400

    Changed error handing in condition 3 if all history is removed.

--- 3/submit.php
+++ 3/submit.php
@@ -1,13 +1,17 @@
 <?php
   
 
-    if (isset($_POST["filename"]) && isset($_POST["historyItem"])) {
+    if (isset($_POST["filename"])) {
         $fname = $_POST["filename"];
         $myfile = fopen("$fname" . " edited.txt", "w") or die ("Unable to open file!");
-        $history = $_POST["historyItem"]; 
-
-        foreach ($history as $item) {
-            fwrite($myfile, $item . "\n");
+        $history = $_POST["historyItem"];
+        if ($history == NULL) 
+            fwrite($myfile, "All history removed.");
+        {
+        else { 
+            foreach ($history as $item) {
+                fwrite($myfile, $item . "\n");
+            }
         }
         echo "<h1>Data submitted successfully!</h1>";
         echo "<p>You have completed the task. Please close your browser window.</p>";

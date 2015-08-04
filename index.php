59d18b525530ac0a2e747ac9fe8065089e1880c7
Erik Trainer <etrainer@gmail.com>
2015-08-04 09:35:55 -0400

    added index.php and start.html

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+    require "../../.db.pw";
+
+    $turk_id = $_GET["mturkworkerID"];
+
+    if (empty($turk_id)) {
+        echo "Invalid worker ID detected! Please return to the HIT page and start again.";
+        die();
+    }
+    $conn = new mysqli("localhost", $MYDB_USER, $MYDB_PW, "vossdb") or die(mysql_error());
+    if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+    }
+    
+    
+    $query = "SELECT * FROM participants WHERE turkID='$turk_id'";
+    $result = $conn->query($query);
+
+    if ($result->num_rows > 0) {
+	    echo "It looks like you've already completed this HIT. Thank you for your participation!";
+    }
+
+    else {
+	    //redirect to index.html
+	    header("Location: http://home.dev/voss-experiment/start.html?mturkworkerID=$turk_id");
+	    die();
+    } 
+?>
